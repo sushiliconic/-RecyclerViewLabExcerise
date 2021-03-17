11a40468169651971e53766691ce02b3fdd375cb
Sushil Ghimire <sushiliconic@gmail.com>
2021-03-17 21:48:59 +0545

    task 2

--- app/src/main/java/com/example/recycleview/MainActivity.java
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.recycleview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -9,6 +11,8 @@ import java.util.LinkedList;
 public class MainActivity extends AppCompatActivity {
 
     private final LinkedList<String> mWordList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,15 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 20; i++) {
             mWordList.addLast("Word " + i);
         }
+
+
+        // Get a handle to the RecyclerView.
+        mRecyclerView = findViewById(R.id.recyclerview);
+// Create an adapter and supply the data to be displayed.
+        mAdapter = new WordListAdapter(this, mWordList);
+// Connect the adapter with the RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+// Give the RecyclerView a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
